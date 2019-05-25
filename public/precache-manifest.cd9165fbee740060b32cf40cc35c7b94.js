4c735c8e32f25bbca08c1d5ff6d93567439a1f4c
Laetronaz <michelwarcraft@hotmail.com>
2019-05-25 14:39:53 -0400

    Finished Course

---
+++ public/precache-manifest.cd9165fbee740060b32cf40cc35c7b94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1a47725d83692488336cbce9e71526a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e66968a2bb777b0c713",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1498844ad20d92fc068b",
+    "url": "/static/js/2.4dbcb935.chunk.js"
+  },
+  {
+    "revision": "6e66968a2bb777b0c713",
+    "url": "/static/js/main.d6a1aa11.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
